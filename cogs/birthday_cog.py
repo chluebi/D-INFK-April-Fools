3f2c72049941c15c02343c398a498f6205a4007b
Karlo <karlo.piskor1995@gmail.com>
2022-03-20 01:51:21 +0100

    Changes

--- cogs/birthday_cog.py
+++ cogs/birthday_cog.py
@@ -5,18 +5,20 @@ import events
 from discord.ext import commands
 from db import SQLiteDBManager
 
+from bot import discord_config
 
 class Birthday(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.db = SQLiteDBManager(discord_config["db_path"])
 
     #when I react with peepolove it gives points
     @commands.Cog.listener()
-    async def on_raw_reaction_add(payload):
+    async def on_raw_reaction_add(self, payload):
         if payload.user_id == 223932775474921472: #id of EvilBabyDemon
             if payload.emoji.name == "peepolove":
                 #transactiontype ID 4 probably will change still
-                SQLiteDBManager.change_credits(payload.member.id, 4)
+                self.db.change_credits(payload.member.id, 4, payload.message_id, payload.channel_id)
             
 
 # this code actually gets run when bot.load_extension(file) gets called on this file
