a2b38d967f4042c0a6bc5b21abca13e903bb2a83
EvilBabyDemon <lteufelbe@ethz.ch>
2022-03-20 02:42:21 +0100

    fixed to correct member

--- cogs/birthday_cog.py
+++ cogs/birthday_cog.py
@@ -16,10 +16,12 @@ class Birthday(commands.Cog):
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         if payload.user_id == 223932775474921472: #id of EvilBabyDemon
-            if payload.emoji.name == "peepolove":
-                #transactiontype ID 4 probably will change still
-                self.db.change_credits(payload.member.id, 4, payload.message_id, payload.channel_id)
-            
+            if payload.member is not None:
+                if payload.emoji.name == "peepolove":
+                    #transactiontype ID 4 probably will change still
+                    messenger = payload.member.guild.get_channel(payload.channel_id).fetch_message(payload.message_id).author
+                    self.db.change_credits(payload.member.guild, 4, payload.message_id, payload.channel_id)
+                
 
 # this code actually gets run when bot.load_extension(file) gets called on this file
 # all cogs that should be loaded need to be added in here
