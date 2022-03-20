ca5bd812ee5714c5c5e5001ed96a1652e0a16839
EvilBabyDemon <lteufebe@ethz.ch>
2022-03-20 23:39:44 +0100

    this should work, cant test because of Marc

--- cogs/reaction_cog.py
+++ cogs/reaction_cog.py
@@ -15,6 +15,9 @@ from constants import TransactionType
 GOOD_EMOTE_IDS = [807968281507659776, 747783377662378004, 810266232061952040, 910435057607524352]
 #                   that                yikes               mike_bruh           bruh
 BAD_EMOTE_IDS = [758262252699779073, 851469747643220048, 937352413810151424, 747783377159061545]
+TA_APPROVED = 893967315996143616
+
+
 
 class Reaction(commands.Cog):
     def __init__(self, bot):
@@ -27,13 +30,18 @@ class Reaction(commands.Cog):
         if payload.member is not None:
             #ReactionUp Transaction
             if any(payload.emoji.id == s for s in GOOD_EMOTE_IDS):
-                messenger = payload.member.guild.get_channel(payload.channel_id).fetch_message(payload.message_id).author
+                messenger = get_author(payload)
                 self.db.change_credits(payload.member, TransactionType.reaction_good, payload.message_id, payload.channel_id)
             elif any(payload.emoji.id == s for s in BAD_EMOTE_IDS):
                 #ReactionDown Transaction
-                messenger = payload.member.guild.get_channel(payload.channel_id).fetch_message(payload.message_id).author
+                messenger = get_author(payload)
                 self.db.change_credits(payload.member, TransactionType.reaction_bad, payload.message_id, payload.channel_id)
-                
+            elif payload.emoji.id == TA_APPROVED:
+                messenger = get_author(payload)
+                self.db.change_credits(payload.member, TransactionType.TAApproved, payload.message_id, payload.channel_id)
+
+def get_author(payload):
+    return payload.member.guild.get_channel(payload.channel_id).fetch_message(payload.message_id).author            
 
 # this code actually gets run when bot.load_extension(file) gets called on this file
 # all cogs that should be loaded need to be added in here
