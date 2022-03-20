69165e52651154073ea907d8be32c3940b724564
Karlo <karlo.piskor1995@gmail.com>
2022-03-20 02:24:13 +0100

    Fix broken DiscordUser

--- db.py
+++ db.py
@@ -4,9 +4,10 @@ import traceback
 
 class DiscordUser(object):
 
-    def __init__(self, discord_user_id, old_name, social_credit, is_bot, is_admin):
+    def __init__(self, discord_user_id, old_name, current_name, social_credit, is_bot, is_admin):
         self.discord_user_id = discord_user_id
         self.old_name = old_name
+        self.current_name = current_name
         self.social_credit = social_credit
         self.is_bot = is_bot
         self.is_admin = is_admin
