2d806a6f4f50c1b3f1d37d95ce1d461c8d9606e0
EvilBabyDemon <lteufelbe@ethz.ch>
2022-03-20 02:58:54 +0100

    calling score_update

--- points.py
+++ points.py
@@ -1,6 +1,7 @@
 import discord
 from db import SQLiteDBManager
 from db import DiscordUser
+from events import score_update
 
 def change_points(member: discord.Member, delta_score: int, reason):
     #update db
@@ -18,4 +19,6 @@ def change_points(member: discord.Member, delta_score: int, reason):
     channel = member.guild.get_channel(954423559600631832)
     update = 'added' if points>0 else 'removed'
     channel.send(f'{member.mention} has gotten {points} {update}. Because of {reason}')
+    
+    score_update(member, user.social_credit)
     return
