c100ce73d6f1430c4fce04d6d985fd6d38747fdd
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-03-27 17:57:36 +0200

    renaming redone

--- cogs/rename_cog.py
+++ cogs/rename_cog.py
@@ -1,5 +1,4 @@
-import asyncio
-
+import re
 import discord
 
 from discord.ext import commands
@@ -18,35 +17,43 @@ class Rename(commands.Cog):
     async def on_member_update(self, before, after):
 
         enable_rename = self.db.get_key("EnforceNameChange")
+        enable_rename = True
 
         if enable_rename is False:
             return
 
         if before.display_name != after.display_name:
             user = self.db.get_discord_user(after.id)
-            score = " [" + str(user.social_credit) + "]"
+            score = f" [{user.social_credit}]"
+
             #if Nickname still has score, dont change but save new nickname into db
-
+            cleaned_nick = re.sub(r"\[\d+\]", "", after.display_name).strip()
             if after.display_name.endswith(score):
-                current = after.display_name
-                current = current.removesuffix(score)
-                self.db.rename_discord_user(after.id, current)
-                return
+                # simply removes all [number] things in a name
+                reduce_credits = False
+                self.db.rename_discord_user(after.id, cleaned_nick)
+            else:
+                reduce_credits = True
+                
+            # we generate the new username. If the previous step cleared it all, we choose a placeholder name
+            if len(cleaned_nick) == 0:
+                cleaned_nick = f"Loyal Citizen #{after.discriminator}"
+            new_nick = cleaned_nick[:25] + score
+            # if the above removal of numbers resulted in a new username. change it
+            if new_nick!= after.display_name:  
+                await after.edit(nick=new_nick)
 
-            await self.db.change_credits(after, TransactionType.invalid_name_change, reason=f"Faulty rename from '{before.nick}' to '{after.nick}'") 
+            if reduce_credits:
+                await self.db.change_credits(after, TransactionType.invalid_name_change, reason=f"Faulty rename from '{before.display_name}' to '{after.display_name}'") 
             
     #updates the user with the correct score again, if there score got updated or they did a faulty rename
     @on_score_update
     async def rename_member(bot, member: discord.Member, user: DiscordUser, delta_score: int, reason):
         # Reload the new credits
-        score = " [" + str(user.social_credit) + "]"
+        score = f" [{user.social_credit}]"
         new_name = user.current_name[:25]+score
-        print(new_name)
         await member.edit(nick=new_name)
             
-            
-
-# this code actually gets run when bot.load_extension(file) gets called on this file
-# all cogs that should be loaded need to be added in here
+        
 def setup(bot: commands.Bot):
     bot.add_cog(Rename(bot))
