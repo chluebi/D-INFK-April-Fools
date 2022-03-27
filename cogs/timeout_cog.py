77e05f7d8f2d9c992697f688ca5454be9775290a
EvilBabyDemon <lteufelbe@ethz.ch>
2022-03-28 00:05:47 +0200

    fixed code

--- cogs/timeout_cog.py
+++ cogs/timeout_cog.py
@@ -9,24 +9,23 @@ from db import DiscordUser
 from constants import TransactionType
 from events import on_score_update, score_update
 
-class Role(commands.Cog):
+class Timeout(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = bot.db
 
             
-    #updates the user with the correct role again after a score change
+    #Gives user timeout
     @on_score_update
     async def timeout(self, member: discord.Member, user: DiscordUser, delta_score: int, reason):
-        if user.social_credit > 0 or member.messenger.guild_permissions.admin:
+        if user.social_credit > 0 or member.guild_permissions.administrator:
             return
-        channel = member.guild.get_channel(954423559600631832)
-        channel.send(f'timeout: {member.id} This is a timeout for {member.mention}')
-        amount = -1 * user.social_credit + 250
-        await self.db.change_credits(member=member, TransactionType=TransactionType.timeout, amount=amount, reason="Timeout")
         
+        channel = member.guild.get_channel(954423559600631832)
+        await channel.send(f'timeout: {member.id} This is a timeout for {member.mention}')
+        amount = -1 * user.social_credit + 250
+        await self.db.change_credits(member, TransactionType.timeout, amount=amount, reason="Timeout")
 
-# this code actually gets run when bot.load_extension(file) gets called on this file
-# all cogs that should be loaded need to be added in here
-def setup(bot: commands.Bot):
-    bot.add_cog(Role(bot))
+## simply here cause its considered a cog
+def setup(bot):
+    pass
