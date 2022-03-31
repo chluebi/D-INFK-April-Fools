5509a812fd545c8d569fe8aaddf3cf35bd390095
EvilBabyDemon <lteufelbe@ethz.ch>
2022-03-31 21:28:01 +0200

    help page may need better words

--- cogs/basic_cog.py
+++ cogs/basic_cog.py
@@ -129,6 +129,16 @@ class Cog(commands.Cog):
     async def addto_blacklist(self, ctx, word):
         Cog.words_1984.add(word)
 
+    
+    @commands.command(name='help')
+    async def help_page(self, ctx):
+        color = discord.Color.green()
+
+        embed = discord.Embed(color=color)
+        embed.add_field(name="transaction", value=f"The last transactions of credits of a User", inline=False)
+        embed.add_field(name="judge", value=f"Change the Credits of a User by 20 (Only useable when you have the Judge Icon", inline=False)
+        await ctx.send(embed=embed)
+
 # this code actually gets run when bot.load_extension(file) gets called on this file
 # all cogs that should be loaded need to be added in here
 def setup(bot: commands.Bot):
