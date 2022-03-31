4b33556e8248f48a3a737a0b8013e9a0f66d5c58
Karlo <karlo.piskor1995@gmail.com>
2022-03-31 23:35:59 +0200

    Add 1000 role on join

--- cogs/guild_join_cog.py
+++ cogs/guild_join_cog.py
@@ -21,7 +21,10 @@ class GuildJoin(commands.Cog):
         if member.bot:
             return
         db_user = self.db.get_discord_user(member.id)
-        
+
+        role = discord.utils.get(member.server.roles, id="957399970636509264")
+        await self.bot.add_roles(member, role)
+
         if db_user is None:
             #TODO check Name and Nick
             self.db.create_discord_user(member.id, member.display_name, 1000, member.bot, False)
